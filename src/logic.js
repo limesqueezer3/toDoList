dc07c91c6ff7664b36782adc4a7032249b46f994
charlie <charliehu2808@outlook.com>
2024-11-21 18:48:25 +0100

    not much

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,12 @@
 import createItem from item.js
 
 let itemList = [];
+let timeStampList = [];
 
+function addItem (item) {
+    itemList.push(item);
+}
+
+function addTimeStamp (timeStamp, day) {
+    timeStampList[day].push(timeStamp);
+}
